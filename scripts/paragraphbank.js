cec51f56d23d7e091cb254745d00acd563061e56
BkCece <ckwright@sfu.ca>
2019-02-09 17:44:05 -0800

    grammar bruh

--- scripts/paragraphbank.js
+++ scripts/paragraphbank.js
@@ -4,8 +4,8 @@
 var paragraphs = [
     {
     	// 0
-        "text": `You wake up from a nap under a tree. The sk’y is clear, some cloud with a black cloud afar, but where you are, it is clear. As you scan your perimeter before you gather your things.  As you get up and % the tree, you see a @ walk over, Do you follow it?`,
-        "left": `Let\'s go!`,
+        "text": `You wake up from a nap under a tree. The sky is clear (a little cloudy with a black cloud afar off) but where you are, it is clear. You scan your perimeter before you gather your things. As you get up and % the tree, you see a @ walk over. Do you follow it?`,
+        "left": `Let's go!`,
         "right" : `Nah.`,
         "nouns": 1,
         "verbs": 1,
@@ -13,8 +13,8 @@ var paragraphs = [
     },
     {
     	// 1
-    	"text": `You realized that that @ stole that attractive neighbor\'s @. You think of that attractive neighbor, but also you remember that you are lost! You unsheath your blade, but hesitate because you may have the ability to talk to the @ and my also gather information on where the heck you are. But your heart beats with justice! You can’t give them the chance! What do you do?`,
-    	"left": `Unsheath thy blade!`,
+    	"text": `You realize that @ stole your attractive neighbor's @. You think of that attractive neighbor, but you also remember that you are lost! You unsheathe your blade but hesitate because you may have the ability to talk to the @ and might also gather information on where the heck you are. But your heart beats with justice! You can't give them the chance! What do you do?`,
+    	"left": `Unsheathe thy blade!`,
     	"right": `I want to know more.`,
     	"nouns": 3,
     	"verbs": 0,
@@ -22,7 +22,7 @@ var paragraphs = [
     },
     {
     	// 2
-    	"text": `You stand up and move along your merry way. You take in the scenery as # @ During your travels, a large @ blocks your path, do you <verb> it?`,
+    	"text": `You stand up and move along your merry way. You take in the scenery as # @. During your travels, a large @ blocks your path, do you % it?`,
     	"left": `Just, do it.`,
     	"right": `Just, NOT do it.`,
     	"nouns": 2,
@@ -31,9 +31,9 @@ var paragraphs = [
     },
     {
     	// 3
-    	"text": `You clench your behind, grab your blade, and leap towards the... @? Gah! That beast! It shapeshifted! Or did it not? No matter! Good thing you were the track team in high school! Your boots digs deeper into the dirt as you’re ready to spring into action. Each step springs and propels you forward as the dust gets left behind your trail. You totally have  super cool sword throwing skills, your @ told you you were very good at! Do you throw the sword?`,
+    	"text": `You clench your behind, grab your blade, and leap towards the... @? Gah! That beast! It shapeshifted! Or did it not? No matter! Good thing you were on the track team in high school! Your boots dig deeper into the dirt as you're ready to spring into action. Each step springs and propels you forward as the dust gets left behind your trail. You totally have super cool sword throwing skills, your @ told you that you were very good at it! Do you throw the sword?`,
     	"left": `Throw`,
-    	"right": `Don\'t throw`,
+    	"right": `Don't throw`,
     	"nouns": 2,
     	"verbs": 0,
     	"adjes": 0
