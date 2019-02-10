e0f1bcc6835c6227761ece03be8a57b6a0186e2f
Guojiaxi <joshliguo@gmail.com>
2019-02-09 18:10:37 -0800

    word bank complete

--- scripts/adjebank.js
+++ scripts/adjebank.js
@@ -1,39 +1,603 @@
 var adjes = [
-    {
-        "adjes": "peaceful"
-    },
-    {
-        "adjes": "ready"
-    },
-    {
-    	"adjes": "jobless"
-    },
-    {
-    	"adjes": "wry"
-    },
-    {
-    	"adjes": "towering"
-    },
-    {
-    	"adjes": "orange"
-    },
-    {
-    	"adjes": "meaty"
-    },
-    {
-    	"adjes": "furtive"
-    },
-    {
-    	"adjes": "phobic"
-    },
-    {
-        "adjes": "wasteful"
-    },
-    {
-        "adjes": "guarded"
-    },
-    {
-        "adjes": "irate"
-    }
+{
+"adje":  "slim"
+},
+{
+"adje":  "brash"
+},
+{
+"adje":  "dusty"
+},
+{
+"adje":  "rebel"
+},
+{
+"adje":  "instinctive"
+},
+{
+"adje":  "frantic"
+},
+{
+"adje":  "melodic"
+},
+{
+"adje":  "rare"
+},
+{
+"adje":  "innocent"
+},
+{
+"adje":  "ancient"
+},
+{
+"adje":  "mundane"
+},
+{
+"adje":  "incredible"
+},
+{
+"adje":  "jolly"
+},
+{
+"adje":  "icy"
+},
+{
+"adje":  "tan"
+},
+{
+"adje":  "deserted"
+},
+{
+"adje":  "quixotic"
+},
+{
+"adje":  "hungry"
+},
+{
+"adje":  "flaky"
+},
+{
+"adje":  "whispering"
+},
+{
+"adje":  "decorous"
+},
+{
+"adje":  "outgoing"
+},
+{
+"adje":  "fair"
+},
+{
+"adje":  "bawdy"
+},
+{
+"adje":  "sore"
+},
+{
+"adje":  "hurried"
+},
+{
+"adje":  "old"
+},
+{
+"adje":  "tawdry"
+},
+{
+"adje":  "incandescent"
+},
+{
+"adje":  "half"
+},
+{
+"adje":  "accurate"
+},
+{
+"adje":  "voiceless"
+},
+{
+"adje":  "yellow"
+},
+{
+"adje":  "absorbed"
+},
+{
+"adje":  "ordinary"
+},
+{
+"adje":  "plastic"
+},
+{
+"adje":  "helpful"
+},
+{
+"adje":  "soft"
+},
+{
+"adje":  "itchy"
+},
+{
+"adje":  "painstaking"
+},
+{
+"adje":  "maddening"
+},
+{
+"adje":  "tightfisted"
+},
+{
+"adje":  "watery"
+},
+{
+"adje":  "tested"
+},
+{
+"adje":  "savory"
+},
+{
+"adje":  "shaggy"
+},
+{
+"adje":  "staking"
+},
+{
+"adje":  "right"
+},
+{
+"adje":  "wanting"
+},
+{
+"adje":  "wealthy"
+},
+{
+"adje":  "magnificent"
+},
+{
+"adje":  "glossy"
+},
+{
+"adje":  "elated"
+},
+{
+"adje":  "literate"
+},
+{
+"adje":  "curved"
+},
+{
+"adje":  "needless"
+},
+{
+"adje":  "breakable"
+},
+{
+"adje":  "guarded"
+},
+{
+"adje":  "tricky"
+},
+{
+"adje":  "neat"
+},
+{
+"adje":  "comfortable"
+},
+{
+"adje":  "cool"
+},
+{
+"adje":  "murky"
+},
+{
+"adje":  "square"
+},
+{
+"adje":  "ready"
+},
+{
+"adje":  "fixed"
+},
+{
+"adje":  "cluttered"
+},
+{
+"adje":  "mean"
+},
+{
+"adje":  "dark"
+},
+{
+"adje":  "unruly"
+},
+{
+"adje":  "ritzy"
+},
+{
+"adje":  "bumpy"
+},
+{
+"adje":  "responsible"
+},
+{
+"adje":  "jobless"
+},
+{
+"adje":  "afraid"
+},
+{
+"adje":  "rabid"
+},
+{
+"adje":  "successful"
+},
+{
+"adje":  "familiar"
+},
+{
+"adje":  "damaged"
+},
+{
+"adje":  "hollow"
+},
+{
+"adje":  "steadfast"
+},
+{
+"adje":  "magenta"
+},
+{
+"adje":  "delicate"
+},
+{
+"adje":  "automatic"
+},
+{
+"adje":  "reminiscent"
+},
+{
+"adje":  "selfish"
+},
+{
+"adje":  "dangerous"
+},
+{
+"adje":  "confused"
+},
+{
+"adje":  "zonked"
+},
+{
+"adje":  "thinkable"
+},
+{
+"adje":  "cautious"
+},
+{
+"adje":  "abashed"
+},
+{
+"adje":  "silent"
+},
+{
+"adje":  "strong"
+},
+{
+"adje":  "stale"
+},
+{
+"adje":  "aboriginal"
+},
+{
+"adje":  "inexpensive"
+},
+{
+"adje":  "dysfunctional"
+},
+{
+"adje":  "crazy"
+},
+{
+"adje":  "busy"
+},
+{
+"adje":  "smart"
+},
+{
+"adje":  "astonishing"
+},
+{
+"adje":  "overjoyed"
+},
+{
+"adje":  "yummy"
+},
+{
+"adje":  "milky"
+},
+{
+"adje":  "bloody"
+},
+{
+"adje":  "penitent"
+},
+{
+"adje":  "quickest"
+},
+{
+"adje":  "statuesque"
+},
+{
+"adje":  "thirsty"
+},
+{
+"adje":  "uttermost"
+},
+{
+"adje":  "past"
+},
+{
+"adje":  "exuberant"
+},
+{
+"adje":  "real"
+},
+{
+"adje":  "sudden"
+},
+{
+"adje":  "economic"
+},
+{
+"adje":  "fumbling"
+},
+{
+"adje":  "nice"
+},
+{
+"adje":  "somber"
+},
+{
+"adje":  "piquant"
+},
+{
+"adje":  "lively"
+},
+{
+"adje":  "gentle"
+},
+{
+"adje":  "puny"
+},
+{
+"adje":  "festive"
+},
+{
+"adje":  "unaccountable"
+},
+{
+"adje":  "warm"
+},
+{
+"adje":  "momentous"
+},
+{
+"adje":  "hard-to-find"
+},
+{
+"adje":  "one"
+},
+{
+"adje":  "puffy"
+},
+{
+"adje":  "physical"
+},
+{
+"adje":  "ashamed"
+},
+{
+"adje":  "fancy"
+},
+{
+"adje":  "gainful"
+},
+{
+"adje":  "fertile"
+},
+{
+"adje":  "holistic"
+},
+{
+"adje":  "disturbed"
+},
+{
+"adje":  "sick"
+},
+{
+"adje":  "nondescript"
+},
+{
+"adje":  "lavish"
+},
+{
+"adje":  "parsimonious"
+},
+{
+"adje":  "zippy"
+},
+{
+"adje":  "green"
+},
+{
+"adje":  "minor"
+},
+{
+"adje":  "tacky"
+},
+{
+"adje":  "nosy"
+},
+{
+"adje":  "arrogant"
+},
+{
+"adje":  "tearful"
+},
+{
+"adje":  "calm"
+},
+{
+"adje":  "capable"
+},
+{
+"adje":  "willing"
+},
+{
+"adje":  "quarrelsome"
+},
+{
+"adje":  "zesty"
+},
+{
+"adje":  "uppity"
+},
+{
+"adje":  "jazzy"
+},
+{
+"adje":  "grieving"
+},
+{
+"adje":  "enormous"
+},
+{
+"adje":  "sweltering"
+},
+{
+"adje":  "obtainable"
+},
+{
+"adje":  "peaceful"
+},
+{
+"adje":  "tidy"
+},
+{
+"adje":  "two"
+},
+{
+"adje":  "unequal"
+},
+{
+"adje":  "imaginary"
+},
+{
+"adje":  "military"
+},
+{
+"adje":  "towering"
+},
+{
+"adje":  "tranquil"
+},
+{
+"adje":  "learned"
+},
+{
+"adje":  "naive"
+},
+{
+"adje":  "brown"
+},
+{
+"adje":  "numerous"
+},
+{
+"adje":  "quirky"
+},
+{
+"adje":  "standing"
+},
+{
+"adje":  "known"
+},
+{
+"adje":  "picayune"
+},
+{
+"adje":  "fantastic"
+},
+{
+"adje":  "oceanic"
+},
+{
+"adje":  "hypnotic"
+},
+{
+"adje":  "giant"
+},
+{
+"adje":  "wise"
+},
+{
+"adje":  "anxious"
+},
+{
+"adje":  "prickly"
+},
+{
+"adje":  "glorious"
+},
+{
+"adje":  "descriptive"
+},
+{
+"adje":  "puzzling"
+},
+{
+"adje":  "thick"
+},
+{
+"adje":  "detailed"
+},
+{
+"adje":  "periodic"
+},
+{
+"adje":  "gratis"
+},
+{
+"adje":  "grumpy"
+},
+{
+"adje":  "drunk"
+},
+{
+"adje":  "safe"
+},
+{
+"adje":  "envious"
+},
+{
+"adje":  "colossal"
+},
+{
+"adje":  "pushy"
+},
+{
+"adje":  "eight"
+},
+{
+"adje":  "energetic"
+},
+{
+"adje":  "internal"
+},
+{
+"adje":  "wrathful"
+},
+{
+"adje":  "combative"
+}
 ]
 var totalAdjes = adjes.length;
