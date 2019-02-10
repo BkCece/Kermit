8fd214ee6992e7d4444a5f153b387518d605b4aa
hceong <jcceong@gmail.com>
2019-02-09 16:10:47 -0800

    butons

--- scripts/paragraphbank.js
+++ scripts/paragraphbank.js
@@ -1,6 +1,8 @@
 var paragraphs = [
     {
         "text": `i love @, and i hate @, but i still love @. I would total %, and % it`,
+        "left": `No`,
+        "right" : `Yes`,
         "nouns": 3,
         "verbs": 2
     }
